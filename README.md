52e3c8210fda2e03dd8a6e6271cf619082a11c63
Mirrorsan <booranasak.k@cs.rmu.ac.th>
2024-10-28 03:12:52 +0700

    Repeat Timer

---
+++ README.md
@@ -0,0 +1,11 @@
+# Repeat 60s Timer
+## React + Vite + Tailwind
+
+**This project Develop by**
+Booranasak Kanthong *Web Developer*
+Thanawan Phanphrom *Ux/Ui Designer*
+
+* function
+    * Timer up to 60 seconds
+    * Accurate Timer
+    * Start/Reset button
---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  let buttonStart = 'bg-[#9eba6e] p-1   pb-2 px-6 m-3 max-w-fit rounded-full text-2xl font-medium'
+  let buttonReset = 'bg-[#da9f88] p-1   pb-2 px-5 m-3 max-w-fit rounded-full text-2xl font-medium'
+  //State for timer
+  const [elapsedSeconds, setElapsedSeconds] = useState(0)
+  const [startTime, setStartTime] = useState(Date.now)
+  //State for Button(true = Start(Timer is stop) / false = Reset(Timer is running ))
+  const [isToggled, setIsToggled] = useState(true)
+
+  // useEffect to manage the timer functionality
+  useEffect(() => {
+    let interval = null;
+    // Check if the timer is active (isToggled is false when the timer is running)
+    if (!isToggled) {
+        interval = setInterval(() => {
+        // Calculate the number of seconds that have passed since the start time
+        const seconds = (Math.floor((Date.now() - startTime)/1000));
+        // Update the elapsed time state
+        setElapsedSeconds(() => {
+          //If more than 60 seconds have pass reset the timer
+          if (seconds > 60) {
+            setStartTime(Date.now)
+            return 0; // Reset Elapse Time to 0
+          }
+          return seconds; // Otherwise, update the elapsed time with the current seconds
+        });
+      }, 1000); //Run interval function every 1 second(1000 miliseconds)
+    }  
+    // Clean up the interval when the component is unmounted or dependencies change
+    return () => {
+      clearInterval(interval);
+    }
+  }, [startTime]) // Restart the effect if startTime changes
+  
+  
+  let handleChange = () => { 
+    //toggle button function
+    setIsToggled(!isToggled);
+    console.log('toggle status: ', isToggled)
+
+    //set timer
+    setStartTime(Date.now()); 
+    setElapsedSeconds(0)
+  }
+  
+  return (
+    <>
+      <div className='content-center h-screen bg-[#020b42] text-white'>
+        <div className='grid grid-cols-1 justify-items-center content-center'>
+          <div className='text-9xl text font-medium mb-2'>{elapsedSeconds}</div>
+          <button onClick={handleChange} className= {isToggled? buttonStart : buttonReset} >{isToggled ? 'Start' : 'Reset'}</button>
+        </div>  
+      </div>
+    </>
+  )
+}
+
+export default App
